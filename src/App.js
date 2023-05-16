d0bcd06edca2696c332f2828732dfe48974c581d
Gagandeep Singh <gaggi.148@gmail.com>
2023-05-16 23:36:59 +0530

    Modified some files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styles from './App.module.css';
+import Image1 from './assets/XOSTLogo.svg';
 
 function App() {
+  console.log("images/XOSTLogo.svg")
   return (
     <div 
       style={{
@@ -9,8 +11,18 @@ function App() {
       className={styles.mainCont}
     >
       <div className={styles.leftCont}>
-        <span className={styles.circle  }/>
-        <image />
+        <span className={styles.shade1}/>
+        <span className={styles.shade2}/>
+        <span className={styles.circle}/>
+        <span className={styles.logo}>
+          <p className={styles.para}>To connect with your DOST!</p>
+        </span>
+        {/* <image 
+          // src={"../public/images/XOSTLogo.svg"}
+          src={Image1}
+          alt="Xost Logo"
+          className={styles.logo}
+        /> */}
       </div>
       <div className={styles.rightCont}></div>
     </div>
