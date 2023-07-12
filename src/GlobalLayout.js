910e3dd0d2b4149e6db290d46e89072b253d9e6c
thisisbatra <batranitin118@gmail.com>
2023-07-13 03:27:52 +0530

    making things clean

--- src/GlobalLayout.js
+++ src/GlobalLayout.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import styles from './App.module.css';
 
-function GlobalLayout({
-  insideLeftContainer,
-  insideRightContainer,
-  bgChange
-}) {
-  return (
-    <div className={`${styles.mainCont} ${bgChange ? styles.mainCont2 : ""}`}>
-      <div className={styles.leftCont}>
-        {insideLeftContainer}
-      </div>
-      <div className={styles.rightCont}>
-        <div className={styles.rightInnerCont}>
-          {insideRightContainer}
-        </div>
-      </div>
-    </div>
+const GlobalLayout=(props)=>{
+  return(
+    <div className={`${styles.mainCont} ${props.bgChange ? styles.mainCont2 : ""}`}>
+           <div className={styles.leftCont}>
+             {props.insideLeftContainer}
+           </div>
+           <div className={styles.rightCont}>
+             <div className={styles.rightInnerCont}>
+               {props.insideRightContainer}
+             </div>
+           </div>
+         </div>
   );
 }
 
